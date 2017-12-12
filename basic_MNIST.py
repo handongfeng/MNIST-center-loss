355808432b28cce7aabfa4cfa39dfc304956c76f
Peter554 <byfield554@gmail.com>
2017-12-12 18:43:31 +0000

    prelu

--- basic_MNIST.py
+++ basic_MNIST.py
@@ -68,9 +68,9 @@ model.compile(optimizer=optim,
               loss=losses.categorical_crossentropy,
               metrics=['accuracy'])
 
-util.build_empty_dir('logs')
+util.build_empty_dir('logs-basic')
 util.build_empty_dir('images-basic')
-call1 = TensorBoard(log_dir='logs')
+call1 = TensorBoard(log_dir='logs-basic')
 call2 = my_callbacks.BasicCall()
 
 model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, verbose=2, validation_data=(x_test, y_test),
