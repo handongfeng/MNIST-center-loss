4121ec7f741efff577da50dcbfaa9ab7af76958e
Peter554 <byfield554@gmail.com>
2017-12-12 15:49:05 +0000

    update basic

--- basic_MNIST.py
+++ basic_MNIST.py
@@ -6,6 +6,7 @@ from keras.layers import Conv2D, MaxPool2D
 from keras import optimizers
 from keras import losses
 import keras
+
 import util
 import my_callback
 
@@ -24,39 +25,40 @@ x_test = x_test.reshape((-1, 28, 28, 1))
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
 
+
 ###
 
 def basic_model(x):
     x = Conv2D(filters=32, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
     x = Activation('relu')(x)
-    x = Conv2D(filters=32, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
-    x = Activation('relu')(x)
+    # x = Conv2D(filters=32, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
+    # x = Activation('relu')(x)
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
     #
     x = Conv2D(filters=64, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
     x = Activation('relu')(x)
-    x = Conv2D(filters=64, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
-    x = Activation('relu')(x)
+    # x = Conv2D(filters=64, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
+    # x = Activation('relu')(x)
     x = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
     #
-    x = Conv2D(filters=128, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
-    x = Activation('relu')(x)
-    x = Conv2D(filters=128, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
-    x = Activation('relu')(x)
-    x = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
+    # x = Conv2D(filters=128, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
+    # x = Activation('relu')(x)
+    # x = Conv2D(filters=128, kernel_size=(5, 5), strides=(1, 1), padding='same')(x)
+    # x = Activation('relu')(x)
+    # x = MaxPool2D(pool_size=(2, 2), strides=(2, 2), padding='valid')(x)
     #
     x = Flatten()(x)
-    side_out = Dense(2, name='side_out')(x)
-    #
-    return Dense(10, activation='softmax')(side_out)
+    x = Dense(2,name='side_out')(x)
+    return Dense(10, activation='softmax')(x)
+
 
 ###
 
-inputs=Input((28,28,1))
-out=basic_model(inputs)
+inputs = Input((28, 28, 1))
+out = basic_model(inputs)
 
 model = Model(inputs=inputs, outputs=out)
-# model.summary()
+model.summary()
 
 optim = optimizers.Adam(lr=initial_learning_rate)
 model.compile(optimizer=optim,
