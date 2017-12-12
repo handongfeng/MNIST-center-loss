6ba5e3021f0672323a80efc814907441418c9eaa
Peter554 <byfield554@gmail.com>
2017-12-12 17:13:17 +0000

    save

--- my_callbacks.py
+++ my_callbacks.py
@@ -23,14 +23,19 @@ class CenterLossCall(Callback):
         labels = np.argmax(data[1], axis=1)
         model = Model(inputs=self.model.input[0], outputs=self.model.get_layer('side_out').output)
         output = model.predict(data[0])
-        visualize(output, labels, epoch)
+        centers = self.model.get_layer('centerlosslayer').get_weights()[0]
+        visualize(output, labels, epoch, centers)
         return
 
 
 class Centers_Print(Callback):
 
-    def on_epoch_end(self, epoch, logs=None):
-        print(self.model.get_layer('centerlosslayer').get_weights())
+    def on_epoch_end(self, epoch, logs={}):
+        print('---')
+        print(type(self.model.get_layer('centerlosslayer').get_weights()))
+        print(len(self.model.get_layer('centerlosslayer').get_weights()))
+        print(self.model.get_layer('centerlosslayer').get_weights()[0])
+        print('---')
 
 
 class ActivateCenterLoss(Callback):
@@ -59,18 +64,17 @@ def visualize_basic(feat, labels, epoch):
         plt.plot(feat[labels == i, 0], feat[labels == i, 1], '.', c=c[i])
     plt.title('epoch = {}'.format(epoch))
     plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
-    # plt.axis('off')
     plt.savefig('./images-basic/epoch-{}-val.png'.format(epoch))
 
 
-def visualize(feat, labels, epoch):
+def visualize(feat, labels, epoch, centers):
     c = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff',
          '#ff00ff', '#990000', '#999900', '#009900', '#009999']
     plt.figure()
     plt.clf()
     for i in range(10):
         plt.plot(feat[labels == i, 0], feat[labels == i, 1], '.', c=c[i])
+    plt.plot(centers[:, 0], centers[:, 1], 'kx', mew=2, ms=4)
     plt.title('epoch = {}'.format(epoch))
     plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
-    # plt.axis('off')
     plt.savefig('./images/epoch-{}-val.png'.format(epoch))
