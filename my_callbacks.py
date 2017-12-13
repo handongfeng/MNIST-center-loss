e566127f4ec87f98cb6b3fad0a8f45989f4dd012
Peter554 <byfield554@gmail.com>
2017-12-13 16:40:30 +0000

    big run

--- my_callbacks.py
+++ my_callbacks.py
@@ -78,7 +78,7 @@ def visualize(feat, labels, epoch, centers, lambda_cl):
     plt.plot(centers[:, 0], centers[:, 1], 'kx', mew=2, ms=4)
     plt.title('Validation data. Lambda_centerloss = {}, Epoch = {}'.format(lambda_cl, epoch))
     plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
-    plt.savefig('./results/epoch-{}-lambda-{}-val.png'.format(lambda_cl, i2str(epoch), lambda_cl))
+    plt.savefig('./results/epoch-{}-lambda-{}-val.png'.format(i2str(epoch), lambda_cl))
     plt.close()
 
 
@@ -92,7 +92,7 @@ def visualize_train(feat, labels, epoch, centers, lambda_cl):
     plt.plot(centers[:, 0], centers[:, 1], 'kx', mew=2, ms=4)
     plt.title('Training data. Lambda_centerloss = {}, Epoch = {}'.format(lambda_cl, epoch))
     plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'], loc='upper right')
-    plt.savefig('./results/epoch-{}-lambda-{}-train.png'.format(lambda_cl, i2str(epoch), lambda_cl))
+    plt.savefig('./results/epoch-{}-lambda-{}-train.png'.format(i2str(epoch), lambda_cl))
     plt.close()
 
 
