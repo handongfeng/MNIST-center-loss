f8b3418e0160e737250514f4c5b6c5f02d4dc713
Peter554 <byfield554@gmail.com>
2017-12-13 15:21:36 +0000

    SGD and clear

--- centerLoss_MNIST.py
+++ centerLoss_MNIST.py
@@ -53,6 +53,7 @@ class CenterLossLayer(Layer):
         super().build(input_shape)
 
     def call(self, x, mask=None):
+
         # x[0] is Nx2, x[1] is Nx10 onehot, self.centers is 10x2
         delta_centers = K.dot(K.transpose(x[1]), (K.dot(x[1], self.centers) - x[0]))  # 10x2
         center_counts = K.sum(K.transpose(x[1]), axis=1, keepdims=True) + 1  # 10x1
@@ -63,7 +64,7 @@ class CenterLossLayer(Layer):
         # self.add_update((self.counter, self.counter + 1), x)
 
         self.result = x[0] - K.dot(x[1], self.centers)
-        self.result = K.sum(self.result ** 2, axis=1, keepdims=True) / K.dot(x[1], center_counts)
+        self.result = K.sum(self.result ** 2, axis=1, keepdims=True) #/ K.dot(x[1], center_counts)
         return self.result # Nx1
 
     def compute_output_shape(self, input_shape):
