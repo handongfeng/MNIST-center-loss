1ffab20b9c53d8da373009f5cb196dfe16e7bf4b
Peter554 <byfield554@gmail.com>
2017-12-13 12:32:25 +0000

    Adam and clear_session

--- centerLoss_MNIST.py
+++ centerLoss_MNIST.py
@@ -46,25 +46,25 @@ class CenterLossLayer(Layer):
                                        shape=(10, 2),
                                        initializer='uniform',
                                        trainable=False)
-        self.counter = self.add_weight(name='counter',
-                                       shape=(1,),
-                                       initializer='zeros',
-                                       trainable=False)  # just for debugging
+        # self.counter = self.add_weight(name='counter',
+        #                                shape=(1,),
+        #                                initializer='zeros',
+        #                                trainable=False)  # just for debugging
         super().build(input_shape)
 
     def call(self, x, mask=None):
-        # # x[0] is Nx2, x[1] is Nx10 onehot, self.centers is 10x2
+        # x[0] is Nx2, x[1] is Nx10 onehot, self.centers is 10x2
         delta_centers = K.dot(K.transpose(x[1]), (K.dot(x[1], self.centers) - x[0]))  # 10x2
         center_counts = K.sum(K.transpose(x[1]), axis=1, keepdims=True) + 1  # 10x1
         delta_centers /= center_counts
         new_centers = self.centers - self.alpha * delta_centers
         self.add_update((self.centers, new_centers), x)
 
-        self.add_update((self.counter, self.counter + 1), x)
+        # self.add_update((self.counter, self.counter + 1), x)
 
         self.result = x[0] - K.dot(x[1], self.centers)
         self.result = K.sum(self.result ** 2, axis=1, keepdims=True) / K.dot(x[1], center_counts)
-        return self.result
+        return self.result # Nx1
 
     def compute_output_shape(self, input_shape):
         return K.int_shape(self.result)
